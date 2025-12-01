e0fa19cbb1b02f72646be4ae45dd0acd629a1292
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-12-01 23:46:14 +0100

    workin stand

--- src/app/components/main/main.ts
+++ src/app/components/main/main.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-main',
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
