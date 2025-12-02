53be9593bb366e3b9b8510f631bee9497508a781
Alexander <aalbrecht@psi.de>
2025-12-02 21:37:38 +0100

    start storage dialog form

--- src/app/components/storage/storage.ts
+++ src/app/components/storage/storage.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule , FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {merge} from 'rxjs';
 
 @Component({
   selector: 'app-storage',
-  imports: [MatDialogModule],
+  imports: [MatDialogModule,FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './storage.html',
   styleUrl: './storage.scss',
 })
 export class Storage {
+  readonly item = new FormControl('', Validators.required);
+  readonly storage = new FormControl('', Validators.required);
 
+  errorMessage = signal('');
+  errorMessageStorage = signal('');
+
+  constructor() {
+    merge(this.item.statusChanges, this.item.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessageItem());
+
+    merge(this.storage.statusChanges, this.storage.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessageStorage());
+  }
+
+  updateErrorMessageItem() {
+    if (this.item.hasError('required')) {
+      this.errorMessage.set('Du musst einen Gegenstand eingeben');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  updateErrorMessageStorage() {
+    if (this.storage.hasError('required')) {
+      this.errorMessageStorage.set('Du musst einen Ort auswählen');
+    } else {
+      this.errorMessageStorage.set('');
+    }
+  }
 }
