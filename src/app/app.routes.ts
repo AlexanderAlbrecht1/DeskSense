a60b61669bb930f55fdf609564dc28b0f03a5516
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-12-07 00:26:39 +0100

    added routes and links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
+import { Imprint } from './components/imprint/imprint';
 import { Main } from './components/main/main';
 import { Routes } from '@angular/router';
+import { PrivacyPolicy } from './components/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   {path: '', component: Main},
+  {path: 'imprint', component: Imprint},
+  {path:'privacy-policy', component: PrivacyPolicy}
 ];
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [MatSlideToggleModule],
+  imports: [MatSlideToggleModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
