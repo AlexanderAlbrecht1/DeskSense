0db98231661b337f75daca56d63fa1ba50cde86f
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-12-06 23:39:57 +0100

    refactor and start implement search

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, signal } from '@angular/core';
+import { DataService } from '../../data.service';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { merge } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
+  ],
   templateUrl: './search.html',
   styleUrl: './search.scss',
 })
-export class Search {
+export class Search implements OnInit {
+  itemsInStorage = [];
+  displayedColumns: string[] = ['item', 'storageLocation', 'storageDetail'];
 
+  readonly searchInput = new FormControl('', Validators.required);
+  errorMessage = signal('');
+
+  constructor(public dataService: DataService, private cdr: ChangeDetectorRef) {
+    merge(this.searchInput.statusChanges, this.searchInput.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessageSearchInput());
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.itemsInStorage = await this.showItemOverview();
+    this.cdr.markForCheck();
+  }
+
+  showItemOverview() {
+    return this.dataService.getItemOverview();
+  }
+
+  updateErrorMessageSearchInput() {
+    if (this.searchInput.hasError('required')) {
+      this.errorMessage.set('Du musst einen Gegenstand eingeben');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
 }
