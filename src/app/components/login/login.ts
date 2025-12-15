c0343bacc407037c03e32df072ab8c799e656dc3
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-12-15 22:34:21 +0100

    start UI login component

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {merge} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, FormsModule, ReactiveFormsModule,MatFormFieldModule,MatInputModule, MatIconModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  readonly password = new FormControl('', [Validators.required]);
+
+  errorMessage = signal('');
+  hide = signal(true);
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 
 }
