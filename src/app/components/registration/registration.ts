e24c537b2c7753d59b77b5ac5badca84fa3abd26
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-12-20 12:18:44 +0100

    start registration

--- src/app/components/registration/registration.ts
+++ src/app/components/registration/registration.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {merge} from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, FormsModule, ReactiveFormsModule,MatFormFieldModule,MatInputModule, MatIconModule, MatTooltipModule, RouterLink],
   templateUrl: './registration.html',
   styleUrl: './registration.scss',
 })
 export class Registration {
+
+  readonly email = new FormControl('', [Validators.required, Validators.email]);
+  readonly password = new FormControl('', [Validators.required]);
+  readonly repeatedPassword = new FormControl('', [Validators.required]);
+
+  errorMessage = signal('');
+  hide = signal(true);
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  register() {
+    console.log('___');
+
+  }
 
 }
